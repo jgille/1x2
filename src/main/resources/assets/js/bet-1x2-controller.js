cfcabab54fdb40dac5e236b96569cc91a6b269ca
Jon Ivmark <jon.ivmark@klarna.com>
2014-09-17 16:38:04 +0200

    Welcome the user..

--- src/main/resources/assets/js/bet-1x2-controller.js
+++ src/main/resources/assets/js/bet-1x2-controller.js
@@ -2,20 +2,63 @@ var app = angular.module('bet1x2');
 
 app.controller('Bet1x2Controller', ['$scope', '$cookies', function ($scope, $cookies) {
 
-    var isUserLoggedIn = function () {
-        var jwt = $cookies.jwt;
-        return jwt !== null;
-    };
-
-    $scope.load = function() {
-        $scope.isLoggedIn = isUserLoggedIn();
+    var getJwt = function () {
+        return $cookies.jwt;
     }
 
-    $scope.greeting = "Hello world!";
+    var isUserLoggedIn = function () {
+        var jwt = getJwt();
+        return typeof jwt !== 'undefined' && jwt !== '';
+    };
+
+    var decodeBase64 = function (s) {
+        var e = {}, i, k, v = [], r = '', w = String.fromCharCode;
+        var n = [
+            [65, 91],
+            [97, 123],
+            [48, 58],
+            [43, 44],
+            [47, 48]
+        ];
+
+        for (z in n) {
+            for (i = n[z][0]; i < n[z][1]; i++) {
+                v.push(w(i));
+            }
+        }
+        for (i = 0; i < 64; i++) {
+            e[v[i]] = i;
+        }
+
+        for (i = 0; i < s.length; i += 72) {
+            var b = 0, c, x, l = 0, o = s.substring(i, i + 72);
+            for (x = 0; x < o.length; x++) {
+                c = e[o.charAt(x)];
+                b = (b << 6) + c;
+                l += 6;
+                while (l >= 8) {
+                    r += w((b >>> (l -= 8)) % 256);
+                }
+            }
+        }
+        return r;
+    };
+
+    $scope.load = function () {
+        $scope.isLoggedIn = isUserLoggedIn();
+
+        if ($scope.isLoggedIn === true) {
+            var token = getJwt();
+            var json = JSON.parse(decodeBase64(token.split('.')[1]));
+            $scope.greeting = 'Välkommen ' + json.username;
+        } else {
+            $scope.greeting = '';
+        }
+    }
 
     $scope.logout = function () {
-        $cookies.jwt = null;
-        $scope.isLoggedIn = false;
+        $cookies.jwt = '';
+        $scope.load();
     };
 
     $scope.load();
