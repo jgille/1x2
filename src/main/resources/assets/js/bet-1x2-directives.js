8f4ff72deb0698cb40c128adeb8879ae516fa076
Jon Ivmark <jon.ivmark@klarna.com>
2014-09-18 10:29:34 +0200

    Random fixes..

--- src/main/resources/assets/js/bet-1x2-directives.js
+++ src/main/resources/assets/js/bet-1x2-directives.js
@@ -13,10 +13,4 @@ app
             templateUrl: '/view/navbar.html'
         };
     })
-    .directive('bet1x2', function () {
-        return {
-            restrict: 'E',
-            templateUrl: '/view/bet-1x2.html'
-        };
-    })
 ;
--- src/main/resources/assets/js/bet-1x2.js
+++ src/main/resources/assets/js/bet-1x2.js
@@ -1,3 +1,19 @@
-var app = angular.module('bet1x2', ['ui.router', 'ngCookies']);
+var app = angular.module('bet1x2', ['ui.router', 'ngCookies', 'ngRoute']);
+
+app.config(function($routeProvider) {
+    $routeProvider
+
+        // route for the home page
+        .when('/', {
+            templateUrl : 'view/home.html',
+            controller  : 'Bet1x2Controller'
+        })
+
+        // route for the about page
+        .when('/results', {
+            templateUrl : 'view/results.html',
+            controller  : 'Bet1x2Controller'
+        });
+});
 
 app.run();
